1d4554699dae4958f36d7f0da17e9f9298d0e485
mbsantiago <santiago.mbal@gmail.com>
2019-11-27 14:59:12 -0600

    updated repository field in setup

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     license='BSD License',
     description='About pages for Selia',
     long_description=README,
-    url='http://selia.conabio.gob.mx/',
+    url='https://github.com/CONABIO-audio/selia-about',
     author='CONABIO, Gustavo Everardo Robredo Esquivelzeta, Santiago Martínez Balvanera',
     author_email='erobredo@conabio.gob.mx, smartinez@conabio.gob.mx',
     classifiers=[
