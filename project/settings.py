88a01e23a479243010bd02797f494792d4a5d72e
mbsantiago <santiago.mbal@gmail.com>
2019-11-29 00:09:13 -0600

    fixed base dir in settings file

--- project/settings.py
+++ project/settings.py
@@ -1,6 +1,13 @@
+import os
+
 from irekua_dev_settings.settings import *
 from selia_templates.settings import *
 from selia_about.settings import *
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale'), ]
 
 
 INSTALLED_APPS = (
@@ -8,4 +15,3 @@ INSTALLED_APPS = (
     SELIA_TEMPLATES_APPS +
     IREKUA_BASE_APPS
 )
-
